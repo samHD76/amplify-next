d78eaf5057727a0fa105e03624d9da86c2626d7e
BabylonCoding <124358490+BabylonCoding@users.noreply.github.com>
2024-12-11 04:07:40 +0000

    spotlight removed

--- app/pages/sponsors/page.tsx
+++ app/pages/sponsors/page.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import Image from "next/image";
 
-import { Spotlight } from '../ui/Spotlight'
+//import { Spotlight } from '../ui/Spotlight'
 
 const page = () => {
   return (
